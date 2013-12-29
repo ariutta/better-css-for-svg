6fc3da3abd547ff7799fc1ad38264904e3ae8e2d
Anders Riutta <git@andersriutta.com>
2013-12-29 15:40:56 -0800

    making progress, although it is still buggy

--- better-css-for-svg.js
+++ better-css-for-svg.js
@@ -1,10 +1,49 @@
 "use strict";
-betterCssForSvg = function(){
+var betterCssForSvg = function(){
 
-  function apply(svg) {
+  // thanks to http://stackoverflow.com/questions/18082/validate-numbers-in-javascript-isnumeric
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  function getPixelValueFromAbbreviation(abbreviation, fontSize) {
+    // 1em = 12pt = 16px = 100%
+    var fontSizeInPixels;
+    if (isNumber(abbreviation)) {
+      return parseFloat(abbreviation);
+    }
+    else {
+      if (abbreviation.toLowerCase().slice(-2) === 'px') {
+        return parseFloat(abbreviation.slice(0, -2));
+      }
+      else {
+        if (abbreviation.toLowerCase().slice(-2) === 'pt') {
+          return parseFloat(abbreviation.slice(0, -2) * (16/12));
+        }
+        else {
+          // TODO can we safely assume fontSize will only be in px or pt?
+          fontSizeInPixels = getPixelValueFromAbbreviation(fontSize, null);
+          if (abbreviation.toLowerCase().slice(-2) === 'em') {
+            return parseFloat(abbreviation.slice(0, -2) * fontSizeInPixels);
+          }
+          else {
+            if (abbreviation.toLowerCase().slice(-1) === '%') {
+              return parseFloat((abbreviation.slice(0, -1) / 100) * fontSizeInPixels);
+            }
+            else {
+              throw new Error('Cannot convert ' + abbreviation + ' to a pixel value.');
+            }
+          }
+        }
+      }
+    }
+  }
+
+  function init(svg) {
     // TODO for now, input svg is the d3 selected svg document node, but this should be updated
     // to handle a selector like "#my-svg" for an in-line SVG as well as the selector for
-    // an SVG inside an object.
+    // an SVG inside an HTML object or embed element. I don't think it will work for an SVG
+    // in an img element.
     //
     // var svg;
     // if (target is a selector for an in-line SVG) {
@@ -21,15 +60,19 @@ betterCssForSvg = function(){
     //   }
     // }
 
-    svg.selectAll('text').each(function(textElement) {
-      var text = d3.select(this).call(betterCssForSvg.text);
+    svg.selectAll('g.text-area').each(function(textElement) {
+      console.log(this);
+      var text = d3.select(this).call(betterCssForSvg.text.apply);
     });
+
+    return 'success';
 
     //svg.selectAll('rect').call(betterCssForSvg.boxModel);
   }
 
   return {
-    apply:apply
+    init:init,
+    getPixelValueFromAbbreviation:getPixelValueFromAbbreviation
   };
 }();
 
--- text.js
+++ text.js
@@ -45,70 +45,85 @@ betterCssForSvg.text = function(){
     return textAnchor;
   };
 
-  function applyTextAlign(d) {
+  function apply(d) {
     console.log('repositioning text');
-    var element = this;
+    console.log(this);
+    var textArea = this;
+    self.mythis = this;
     var dx, dy;
-    var computedStyle = window.getComputedStyle(element);
+    var computedStyle = window.getComputedStyle(textArea[0][0]);
     var textAlign = computedStyle.textAlign;
     var verticalAlign = computedStyle.verticalAlign;
+
     var fontSize = computedStyle.fontSize;
-    var bBox = element.getBBox()
-    var parent = element.parentElement;
-    parent.bBox = parent.getBBox();
+    var fontSizeInPixels = betterCssForSvg.getPixelValueFromAbbreviation(computedStyle.fontSize);
+
+    var bBox = textArea[0][0].getBBox()
+
+    var parentElement = textArea[0][0].parentElement;
+    var parentElementBBox = parentElement.getBBox();
+    var parentElementComputedStyle = window.getComputedStyle(parentElement);
+    var parentElementPadding = parentElementComputedStyle.padding;
+    var parentElementPaddingInPixels = betterCssForSvg.getPixelValueFromAbbreviation(parentElementComputedStyle.padding, fontSizeInPixels);
 
     // tweak left, center, right horizontal alignment
-    // giving padding of 5. maybe this should go into the CSS.
+    // using padding of parentElement.
 
     if (textAlign === 'left') {
-      dx = 5 + (-1)*bBox.x;
+      dx = parentElementPaddingInPixels + (-1)*bBox.x;
     }
     else {
       if (textAlign === 'right') {
-        dx = parent.bBox.width - (5 + bBox.width) + (-1)*bBox.x;
+        dx = parentElementBBox.width - (parentElementPaddingInPixels + bBox.width) + (-1)*bBox.x;
       }
       else {
-        dx = parent.bBox.width / 2 + (-1)*bBox.x;
+        dx = parentElementBBox.width / 2 + (-1)*bBox.x;
       }
     }
 
-    //var textAnchor = getTextAnchor(element.tspan[0], textAlign);
-    //element.style.textAnchor = textAnchor;
+    //var textAnchor = getTextAnchor(textArea.tspan[0], textAlign);
+    //textArea.style.textAnchor = textAnchor;
 
     // set top, middle, bottom vertical alignment
 
-    if (!!verticalAlign) {
-      if (verticalAlign === 'top') {
-        dy = 5 + bBox.height + (-1)*bBox.y;
-      }
-      else {
-        if (verticalAlign === 'bottom') {
-          dy = parent.bBox.height - (5 + bBox.height) + (-1)*bBox.y;
-        }
-        else {
-          dy = ((parent.bBox.height / 2) + bBox.height/2) + (-1)*bBox.y;
-        }
-      }
+    // see http://www.w3.org/TR/2011/REC-CSS2-20110607/visudet.html#propdef-vertical-align
+    // baseline | sub | super | top | text-top | middle | bottom | text-bottom | <percentage> | <length> | inherit
+    //
+    // TODO Baseline refers to where text "sits." A vertical-align value of "baseline" is approximately equal to 
+    // a value of "bottom," but not exactly. This and the other vertical align values can be tweaked to be more
+    // correct.
+    //
+    // http://css-tricks.com/what-is-vertical-align/
+    if (verticalAlign === 'middle') {
+      dy = ((parentElementBBox.height / 2) + bBox.height/2) + (-1)*bBox.y;
     }
     else {
-      dy = ((parent.bBox.height / 2) + bBox.height/2) + (-1)*bBox.y;
+      if (verticalAlign === 'top' || verticalAlign === 'super' || verticalAlign === 'text-top') {
+        dy = parentElementPaddingInPixels + bBox.height + (-1)*bBox.y;
+      }
+      else {
+        if (verticalAlign === 'bottom' || verticalAlign === 'baseline' || verticalAlign === 'sub' || verticalAlign === 'text-bottom') {
+          dy = parentElementBBox.height - (parentElementPaddingInPixels + bBox.height) + (-1)*bBox.y;
+        }
+        else {
+          dy = 0;
+          console.warn('verticalAlign property name not recognized.');
+        }
+      }
     }
 
-    var nodeText = d3.select(element) 
-    .attr('transform', function(d) {
-      return 'translate(' + dx + ' ' + dy + ')';
-    });
+    textArea.attr('transform', 'translate(' + dx + ' ' + dy + ')');
 
-    var tSpanComputedStyle, tSpanBBox;
-    var tSpans = nodeText.selectAll('tspan').map(function(tSpan) {
-      tSpanComputedStyle = window.getComputedStyle(tSpan[0]);
-      tSpanBBox = tSpan[0].getBBox();
-      d3.select(this).attr("y", function (d, i) { return i * tSpanBBox.height;})
+    var textLineComputedStyle, textLineBBox;
+    var textLines = textArea.selectAll('text').map(function(textLine) {
+      textLineComputedStyle = window.getComputedStyle(textLine[0]);
+      textLineBBox = textLine[0].getBBox();
+      d3.select(this).attr("y", function (d, i) { return i * textLineBBox.height;})
     });
   }
 
   return {
-    applyTextAlign:applyTextAlign
+    apply:apply
   };
 }();
 
